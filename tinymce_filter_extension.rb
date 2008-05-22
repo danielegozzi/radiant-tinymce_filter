3a2606b5eebb84345e8bbb1fdc6523391812b91e
Andrea Franz <andrea@gravityblast.com>
2008-05-22 22:53:49 +0200

    removed duplicated code on TinymceFilterExtension

--- tinymce_filter_extension.rb
+++ tinymce_filter_extension.rb
@@ -11,22 +11,11 @@ class TinymceFilterExtension < Radiant::Extension
     
     # Add the appropriate stylesheets to the javascripts array in the page and snippet controller
     Admin::PageController.class_eval do
-      before_filter :add_tinymce_javascripts, :only => [:edit]
-      
+      before_filter :add_tinymce_javascripts, :only => [:edit]      
       def add_tinymce_javascripts
         @javascripts << 'extensions/tiny_mce/tiny_mce' << 'extensions/tiny_mce/tiny_mce_settings'
-      end
-      
-    end
-    
-    Admin::SnippetController.class_eval do
-      before_filter :add_tinymce_javascripts, :only => [:edit]
-      
-      def add_tinymce_javascripts
-        @javascripts << 'extensions/tiny_mce/tiny_mce' << 'extensions/tiny_mce/tiny_mce_settings'
-      end
-      
-    end
+      end      
+    end        
     
   end
   
