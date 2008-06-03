9885e08dfe0e3e567430354ccfa6e76dffed75c6
Daniele Gozzi <daniele.gozzi@gmail.com>
2008-06-03 23:57:00 +0200

    Added tinymce also for new page.
    Conflicts:
    
    	tinymce_filter_extension.rb

--- tinymce_filter_extension.rb
+++ tinymce_filter_extension.rb
@@ -11,8 +11,8 @@ class TinymceFilterExtension < Radiant::Extension
     
     # Add the appropriate stylesheets to the javascripts array in the page and snippet controller
     Admin::PageController.class_eval do
-      before_filter :add_tinymce_javascripts, :only => [:edit]
-      
+      before_filter :add_tinymce_javascripts, :only => [:new, :edit]
+      private
       def add_tinymce_javascripts
         @javascripts << 'extensions/tiny_mce/tiny_mce' << 'extensions/tiny_mce/tiny_mce_settings'
       end
